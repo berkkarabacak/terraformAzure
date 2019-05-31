8a481a04ce5aba20a47e833dd9b1db07aca2fc7f
berkkarabacak <berk.karabacak@thermofisher.com>
2019-05-31 11:31:50 +0300

    comments deleted

--- main.tf
+++ main.tf
@@ -17,13 +17,6 @@ resource "azurerm_subnet" "test" {
  address_prefix       = "10.0.2.0/24"
 }
 
-# resource "azurerm_public_ip" "publicIPForLB" {
-#  name                         = "publicIPForLB"
-#  location                     = "${azurerm_resource_group.test.location}"
-#  resource_group_name          = "${azurerm_resource_group.test.name}"
-#  allocation_method            = "Static"
-# }
-
 resource "azurerm_public_ip" "ipforbackendvm" {
  count                        = "${var.redundancy_count}"
  name                         = "ipforbackendvm_${count.index}"
@@ -32,22 +25,6 @@ resource "azurerm_public_ip" "ipforbackendvm" {
  allocation_method            = "Static"
 }
 
-# resource "azurerm_lb" "test" {
-#  name                = "loadBalancer"
-#  location            = "${azurerm_resource_group.test.location}"
-#  resource_group_name = "${azurerm_resource_group.test.name}"
-
-#  frontend_ip_configuration {
-#    name                 = "publicIPAddress"
-#    public_ip_address_id = "${azurerm_public_ip.publicIPForLB.id}"
-#  }
-# }
-
-# resource "azurerm_lb_backend_address_pool" "test" {
-#  resource_group_name = "${azurerm_resource_group.test.name}"
-#  loadbalancer_id     = "${azurerm_lb.test.id}"
-#  name                = "BackEndAddressPool"
-# }
 
 resource "azurerm_network_security_group" "test" {
   name                = "acceptanceTestSecurityGroup1"
@@ -83,16 +60,8 @@ resource "azurerm_network_interface" "test" {
    subnet_id                     = "${azurerm_subnet.test.id}"
    private_ip_address_allocation = "dynamic"
    public_ip_address_id           = "${element(azurerm_public_ip.ipforbackendvm.*.id, count.index)}"
-  #  load_balancer_backend_address_pools_ids = ["${azurerm_lb_backend_address_pool.test.id}"]
  }
 }
-
-# resource "azurerm_network_interface_backend_address_pool_association" "test" {
-#   count                   = "${var.redundancy_count}"
-#   network_interface_id    = "${element(azurerm_network_interface.test.*.id, count.index)}"
-#   ip_configuration_name   = "testconfiguration${count.index}"
-#   backend_address_pool_id = "${azurerm_lb_backend_address_pool.test.id}"
-# }
 
 resource "azurerm_managed_disk" "test" {
  count                = "${var.redundancy_count}"
@@ -103,15 +72,6 @@ resource "azurerm_managed_disk" "test" {
  create_option        = "Empty"
  disk_size_gb         = "1023"
 }
-
-# resource "azurerm_availability_set" "avset" {
-#  name                         = "${var.prefix}_avset"
-#  location                     = "${azurerm_resource_group.test.location}"
-#  resource_group_name          = "${azurerm_resource_group.test.name}"
-#  platform_fault_domain_count  = 2
-#  platform_update_domain_count = 2
-#  managed                      = true
-# }
 
 resource "azurerm_virtual_machine" "test" {
  count                 = "${var.redundancy_count}"
@@ -142,25 +102,8 @@ resource "azurerm_virtual_machine" "test" {
    managed_disk_type = "Standard_LRS"
  }
 
- # Optional data disks
-#  storage_data_disk {
-#    name              = "datadisk_new_${count.index}"
-#    managed_disk_type = "Standard_LRS"
-#    create_option     = "Empty"
-#    lun               = 0
-#    disk_size_gb      = "1023"
-#  }
-
-#  storage_data_disk {
-#    name            = "${element(azurerm_managed_disk.test.*.name, count.index)}"
-#    managed_disk_id = "${element(azurerm_managed_disk.test.*.id, count.index)}"
-#    create_option   = "Attach"
-#    lun             = 1
-#    disk_size_gb    = "${element(azurerm_managed_disk.test.*.disk_size_gb, count.index)}"
-#  }
-
  os_profile {
-   computer_name  = "hostname"
+   computer_name  = "hostname1"
    admin_username = "${var.admin_username}"
    admin_password = "${var.admin_password}"
  }
@@ -179,7 +122,6 @@ resource "azurerm_virtual_machine" "test" {
       user     = "${var.admin_username}"
       password = "${var.admin_password}"
       }
-
 }
 
 resource "null_resource" "assets" {
@@ -191,8 +133,6 @@ resource "null_resource" "assets" {
     vm_instance_ids = "${element(azurerm_virtual_machine.test.*.id,0)}"
   }
 
-  # Bootstrap script can run on any instance of the cluster
-  # So we just choose the first in this case
   connection {
       type     = "ssh"
       host     = "${element(azurerm_public_ip.ipforbackendvm.*.ip_address,0)}"
@@ -213,13 +153,10 @@ resource "null_resource" "assets" {
 resource "null_resource" "quote" {
     depends_on = ["azurerm_virtual_machine.test" ]
 
-  # Changes to any instance of the vms requires re-provisioning
   triggers = {
     vm_instance_ids = "${element(azurerm_virtual_machine.test.*.id,1)}"
   }
 
-  # Bootstrap script can run on any instance of the cluster
-  # So we just choose the first in this case
   connection {
       type     = "ssh"
       host     = "${element(azurerm_public_ip.ipforbackendvm.*.ip_address,1)}"
@@ -237,24 +174,18 @@ resource "null_resource" "quote" {
       }
 }
 
-
 resource "null_resource" "Newsfeed" {
     depends_on = ["azurerm_virtual_machine.test"]
 
-  # Changes to any instance of the vms requires re-provisioning
   triggers = {
     vm_instance_ids = "${element(azurerm_virtual_machine.test.*.id,2)}"
   }
-
-  # Bootstrap script can run on any instance of the cluster
-  # So we just choose the first in this case
   connection {
       type     = "ssh"
       host     = "${element(azurerm_public_ip.ipforbackendvm.*.ip_address,2)}"
       user     = "${var.admin_username}"
       password = "${var.admin_password}"
       }
-
        provisioner "remote-exec" {
         inline = [
           "git clone https://github.com/berkkarabacak/microservicedemo.git",
@@ -265,17 +196,13 @@ resource "null_resource" "Newsfeed" {
       }
 }
 
-
 resource "null_resource" "frontend" {
     depends_on = ["azurerm_virtual_machine.test"  ]
 
-  # Changes to any instance of the vms requires re-provisioning
   triggers = {
     vm_instance_ids = "${element(azurerm_virtual_machine.test.*.id,3)}"
   }
 
-  # Bootstrap script can run on any instance of the cluster
-  # So we just choose the first in this case
   connection {
       type     = "ssh"
       host     = "${element(azurerm_public_ip.ipforbackendvm.*.ip_address,3)}"
